e909bc72321c405037dcd40ecaf171918d30e624
Kamran92 <k.r.a.92mail.ru>
2020-06-19 17:41:44 +0300

    Updates

---
+++ precache-manifest.12fcbb0833344508bb166f3779ad581f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "449b9f8c2833ffd96c53",
+    "url": "/game-of-thrones-DB/static/css/main.1f70f6bc.chunk.css"
+  },
+  {
+    "revision": "449b9f8c2833ffd96c53",
+    "url": "/game-of-thrones-DB/static/js/main.449b9f8c.chunk.js"
+  },
+  {
+    "revision": "7942467dd9a6fea5951d",
+    "url": "/game-of-thrones-DB/static/css/1.52884d75.chunk.css"
+  },
+  {
+    "revision": "7942467dd9a6fea5951d",
+    "url": "/game-of-thrones-DB/static/js/1.7942467d.chunk.js"
+  },
+  {
+    "revision": "8b55067b17a5900115b1",
+    "url": "/game-of-thrones-DB/static/js/runtime~main.8b55067b.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/game-of-thrones-DB/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/game-of-thrones-DB/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "fd3b79d86b4b58b5433f3f5911203be2",
+    "url": "/game-of-thrones-DB/index.html"
+  }
+];
